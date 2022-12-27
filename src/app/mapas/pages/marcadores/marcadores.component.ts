01fdaf7813d72ed74685164ba5168aa37feaa9b0
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2022-12-27 07:31:02 -0500

    marcador en el mapa

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
   styles: [
-  ]
+    `
+      .mapa-container {
+        height: 100%;
+        width: 100%;
+      }
+    `,
+  ],
 })
-export class MarcadoresComponent {
+export class MarcadoresComponent implements AfterViewInit {
+  @ViewChild('mapa') divMapa!: ElementRef;
+  mapa!: mapboxgl.Map;
+  zoomLevel: number = 15;
+  center: [number, number] = [-79.19864771812922, -3.9870315009066095];
 
+  ngAfterViewInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center,
+      zoom: this.zoomLevel,
+    });
+
+    // const markerHtml: HTMLElement = document.createElement('div');
+    // markerHtml.innerHTML = 'Hello World\n↓'; // <--- This is the text that will appear on the marker and it customizes the marker with any HTML you want.
+
+    // const marker = new mapboxgl.Marker({ element: markerHtml })
+    const marker = new mapboxgl.Marker()
+      .setLngLat(this.center)
+      .addTo(this.mapa);
+  }
 }
