d3fdd1419a28a68606a371eb14b92c11da198c65
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2022-12-26 19:00:49 -0500

    Agregar mapbox and show a mapa full-screen

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  mapboxToken: 'pk.eyJ1IjoiZjRiaWFhbiIsImEiOiJjbGM0MzJtcjk0dXJ0M3BwbDM3d2dqMjF4In0.QANX6AHFpGAEmtqUmpIJ3w',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  mapboxToken: 'pk.eyJ1IjoiZjRiaWFhbiIsImEiOiJjbGM0MzJtcjk0dXJ0M3BwbDM3d2dqMjF4In0.QANX6AHFpGAEmtqUmpIJ3w',
+};
